3bb6fef5d06c855693447c2931a713d1ee45480c
Pete Swan <pete@indabamusic.com>
2011-02-18 22:28:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Searchandrestore::Application.config.secret_token = 'd964d6198bc262a097a847e23dee54211230adb5d906153b001308286dfb4922404fd3711394f137fde004187fabf983d3a04fe9a33db2ad36c56ef9d4b7ac0c'
