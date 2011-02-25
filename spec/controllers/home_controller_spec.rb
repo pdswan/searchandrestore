a7a831d84d176718b1de0c3f8b1cebdf0fd8327d
Pete Swan <pete@indabamusic.com>
2011-02-25 01:18:01 -0500

    updates home_controller_spec.rb

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe HomeController do
-
+  describe 'GET index' do
+    it "renders the index template" do
+      get :index
+      response.should render_template('index')
+    end
+  end
 end
