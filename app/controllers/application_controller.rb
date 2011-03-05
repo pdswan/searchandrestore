49f2b3676dfffc6890131ffffc76ddca5ea40d29
Pete Swan <pete@indabamusic.com>
2011-03-05 17:01:52 -0500

    ApplicationController handles CanCan::AccessDenied exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    if anybody_signed_in?
+      redirect_to root_url, :alert => exception.message
+    else
+      redirect_to new_user_session_path, :alert => exception.message
+    end
+  end
 end
