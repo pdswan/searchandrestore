569f798d9e15534ec269305ba1df3e49429a2b20
Dalton Ridenhour <dalton@indabamusic.com>
2011-05-21 15:22:45 -0400

    moves gmap_key out of config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Searchandrestore::Application.initialize!
+
+GMAP_KEY = 'ABQIAAAAgWjvq5VmPsi62Hq7CCASJxQwNLt4fLk2XZHTz1FQBq5OyejVHxQU1DwHBBkyacTZedMqdmGUZNspAA'
