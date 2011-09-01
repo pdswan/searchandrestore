a43405a071290864d0387a4b0f2b3e978cd5d6ba
Pete Swan <pete@indabamusic.com>
2011-08-31 23:57:23 -0400

    adds Artist#associated_artists

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,25 @@ describe Artist do
 
     it { should validate_presence_of(:bio) }
   end
+
+  context "there are 2 artists which share a show" do
+    let(:artist) { Factory(:live_artist_with_upcoming_shows) }
+    let(:associate) { Factory(:live_artist) }
+    let(:non_associate) { Factory(:live_artist) }
+    
+    before do
+      Factory(:performance, :artist => associate,
+                            :show   => artist.upcoming_shows.first)
+    end
+
+    describe "#associated_artists" do
+      it "should include associate" do
+        artist.associated_artists.should include(associate)
+      end
+
+      it "should not include non-associate" do
+        artist.associated_artists.should_not include(non_associate)
+      end
+    end
+  end
 end
