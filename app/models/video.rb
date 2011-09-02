a25df1767f35233733ec48a80c3a299f1edaa946
Pete Swan <pete@indabamusic.com>
2011-09-02 03:40:26 -0400

    adds class method to update existing embed codes. adds oembed options.

--- app/models/video.rb
+++ app/models/video.rb
@@ -20,22 +20,24 @@ class Video < ActiveRecord::Base
   mount_uploader :thumbnail, VideoThumbnailUploader
 
   def self.oembed_options
-    { :width  => 651,
-      :height => 350 }
+    { :width  => 621,
+      :height => 350,
+      :title  => 0,
+      :byline => 0,
+      :color  => 'ffffff' }
+  end
+
+  def self.update_existing_embed_codes
+    find_each do |video|
+      video.fetch_oembed
+      video.save
+    end
   end
 
   def url=(url)
     ret = write_attribute(:url, url)
-
-    if ret && url_changed?
-      require 'oembed'
-
-      if resource = OEmbed::Providers::Vimeo.get(oembed_request_url)
-        self.title                = resource.title if self.title.blank?
-        self.embed_code           = resource.html
-        self.remote_thumbnail_url = resource.thumbnail_url
-      end
-    end
+    
+    fetch_oembed if ret && url_changed?
 
     ret
   end
@@ -47,5 +49,15 @@ class Video < ActiveRecord::Base
 
   def oembed_request_url
     url + '?' + self.class.oembed_options.to_query if url.present?
+  end
+
+  def fetch_oembed
+    require 'oembed'
+
+    if resource = OEmbed::Providers::Vimeo.get(oembed_request_url)
+      self.title                = resource.title if self.title.blank?
+      self.embed_code           = resource.html
+      self.remote_thumbnail_url = resource.thumbnail_url
+    end
   end
 end
