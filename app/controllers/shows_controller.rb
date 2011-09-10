59a2e64d55a521017be9d8d7226215175b451c11
Pete Swan <pete@indabamusic.com>
2011-09-10 15:00:17 -0400

    adds includes for prefetching necessary data in shows controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,11 +3,16 @@ class ShowsController < ApplicationController
 
   def index
     @uses_datepicker = true
-    @shows           = Show.search(params[:search])
+    @shows           = Show.includes([:venue, { :performances => [:artist, :instrument] }]).
+                            search(params[:search])
+
     @current_date    = params[:search][:for_day].respond_to?(:strftime) ?
       params[:search][:for_day] : Time.zone.parse(params[:search][:for_day])
 
-    @upcoming_search_and_restore_shows = Show.upcoming.search_and_restore.limit(5)
+    @upcoming_search_and_restore_shows = Show.upcoming.
+                                              search_and_restore.
+                                              limit(5).
+                                              includes(:venue)
   end
 
   protected
