e0834b0f782ef0f74e7e1594261bb8ac5fef26c9
Pete Swan <pete@indabamusic.com>
2011-09-13 01:52:13 -0400

    removes unecessary "show me the page" from scenario

--- features/step_definitions/venue_steps.rb
+++ features/step_definitions/venue_steps.rb
@@ -2,7 +2,6 @@ Then /^I should see the venues?$/ do
   Venue.all.each do |v|
     Then %{I should see "#{v.name}"}
     Then %{I should see "#{v.city}"}
-    Then "show me the page"
     Then %{I should see a link to "#{url_for(v)}"}
   end
 end
