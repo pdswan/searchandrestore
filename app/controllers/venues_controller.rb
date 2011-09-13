5e0bc3b86ce7f21b25449c47af42b9638b0cdbc8
Pete Swan <pete@indabamusic.com>
2011-09-13 01:28:22 -0400

    user can navigate to the venue detail page from the venue select box on the venue landing page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,20 @@
 class VenuesController < ApplicationController
+  before_filter :redirect_to_venue_detail, :only => :index
 
   def index
     @uses_gmap = true
     @venues    = Venue.scoped
   end
 
+  def show
+    @venue = Venue.find(params[:id])
+    render :nothing => true
+  end
+
+  protected
+
+    def redirect_to_venue_detail
+      return true unless params[:id].present?
+      redirect_to venue_path(params[:id])
+    end
 end
--- features/venues.feature
+++ features/venues.feature
@@ -1,7 +1,14 @@
 Feature: Venues
 
-  @wip
   Scenario: Visitor sees venues
     Given a venue exists
     When I go to the venues page
     Then I should see the venue
+
+  @wip
+  Scenario: Visitor searches for venue
+    Given a venue exists with a name of "CBGBs"
+    When I go to the venues page
+    And I select "CBGBs" from "Name:"
+    Then I press "search"
+    Then I should be on the detail page for the venue
