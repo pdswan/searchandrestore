6c94248721c88d77586977c5ac237e385256bda8
Pete Swan <pete@indabamusic.com>
2011-09-29 01:38:24 -0400

    admin creates press clipping with link. admin can't create press clipping without required fields.

--- features/step_definitions/press_clipping_steps.rb
+++ features/step_definitions/press_clipping_steps.rb
@@ -1,4 +1,7 @@
 Then /^I fill in the new press clipping form with valid attributes$/ do
   Then %{I fill in "Source" with "New York Times"}
   And  %{I fill in "Author" with "Dave Thomas"}
+  And  %{I choose the date "2011-09-25" from "press_clipping_date"}
+  And  %{I fill in "Title" with "Article about bowel movements"}
+  And  %{I fill in "Url" with "http://www.google.com"}
 end
--- spec/models/press_clipping_spec.rb
+++ spec/models/press_clipping_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe PressClipping do
   it { should validate_presence_of(:source) }
   it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:link) }
 end
