df8f16f8bd50868c06703a8ad8f7efc595b587c3
Pete Swan <pete@indabamusic.com>
2011-10-04 23:43:47 -0400

    user sees featured video on homepage

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -1,4 +1,8 @@
 Then /^I choose the video as the featured homepage video$/ do
-  Then %{show me the page}
   Then %{I select "#{the.video.title}" from "Video"}
+end
+
+Given /^the video is featured on the homepage$/ do
+  the.homepage = Homepage.last || Homepage.create
+  the.homepage.update_attribute(:video_id, the.video.id)
 end
