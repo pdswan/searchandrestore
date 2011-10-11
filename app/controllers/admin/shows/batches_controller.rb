75a113a754f9ba46885b09afcd0df9947fbd3ece
Pete Swan <pete@indabamusic.com>
2011-10-11 08:32:17 -0400

    admin can stop batch entry

--- app/controllers/admin/shows/batches_controller.rb
+++ app/controllers/admin/shows/batches_controller.rb
@@ -4,4 +4,9 @@ class Admin::Shows::BatchesController < ApplicationController
     redirect_to new_admin_show_path(:start_batch => true)
   end
 
+  def destroy
+    session[:batch].delete(:show)
+    redirect_to :back, :notice => 'You have exited batch mode.'
+  end
+
 end
--- features/step_definitions/shows/batch_entry_steps.rb
+++ features/step_definitions/shows/batch_entry_steps.rb
@@ -1,12 +1,22 @@
-Then /^I create a new batch entry session$/ do
-  Then %{I follow "Start batch"}
+Then /^I create a batch entry session$/ do
+  Then %{I follow "Start batch entry"}
   And  %{I select "#{the.venue.name}" from "Venue"}
   And  %{I fill in "Group name" with "Batch group name"}
   And  %{I fill in "Description" with "Batch description"}
   And  %{I press "Start"}
 end
 
+Given /^I have created a batch entry session$/ do
+  Then %{I go to the admin shows page}
+  And  %{I create a batch entry session}
+end
+
 Then /^the fields should be populated with the data from the batch entry session$/ do
   Then %{the "Group name" field should contain "Batch group name"}
   Then %{the "Description" field should contain "Batch description"} 
+end
+
+Then /^the fields should not be populated with any data$/ do
+  find_field("Group name").value.should be_blank
+  find_field("Description").value.should be_blank
 end
