aff88e79d4904127e14a8e0acaf604bceb386233
Pete Swan <pete@indabamusic.com>
2011-10-16 15:00:36 -0400

    adds performance link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def performance_link(performance, *options)
+    link_to_if(performance.artist.live?,
+               "#{performance.artist.name}(#{performance.instrument.present? ? performance.instrument.name : performance.artist.instrument.name})", 
+               performance.artist,
+               *options)
+  end
+
 end
