4a34ea78cb8c77d4439e740c8bb11df6e4195254
Pete Swan <pete@indabamusic.com>
2011-10-17 10:55:58 -0400

    fixes performances link for case when artist does not have an instrument

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
 
   def performance_link(performance, *options)
+    instrument = if performance.instrument.present?
+                   performance.instrument.name
+                 elsif performance.artist.instrument.present?
+                   performance.artist.instrument.name
+                 end
+
     link_to_if(performance.artist.live?,
-               "#{performance.artist.name}(#{performance.instrument.present? ? performance.instrument.name : performance.artist.instrument.name})", 
+               "#{performance.artist.name}#{instrument ? "(#{instrument})" : ""}", 
                performance.artist,
                *options)
   end
