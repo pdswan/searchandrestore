c89c60e41a34283f1b3cd8e5196aae35fad2254a
Pete Swan <pete@indabamusic.com>
2011-10-21 01:49:08 -0400

    search fields on shows page are exclusive. searches for artists and venues shows all upcoming shows.

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,14 +1,15 @@
 class ShowsController < ApplicationController
-  before_filter :set_date
+  before_filter :set_default_date
+  before_filter :set_current_date
+  before_filter :set_search_title
 
   def index
     @uses_datepicker = true
     @shows           = Show.includes([:venue, { :performances => [:artist, :instrument] }]).
-                            order("shows.when DESC").
-                            search(params[:search])
+                            order("shows.when ASC")
 
-    @current_date    = params[:search][:for_day].respond_to?(:strftime) ?
-      params[:search][:for_day] : Time.zone.parse(params[:search][:for_day])
+    @shows = @shows.upcoming unless @current_date
+    @shows = @shows.search(params[:search])
 
     @upcoming_search_and_restore_shows = Show.upcoming.
                                               search_and_restore.
@@ -18,10 +19,35 @@ class ShowsController < ApplicationController
 
   protected
 
-    def set_date
+    def set_default_date
+      return true unless empty_search?
+
       params[:search] ||= { }
-      if params[:search][:for_day].blank?
-        params[:search][:for_day] = Time.now
+      params[:search][:for_day] = Time.now
+    end
+
+    def set_current_date
+      if params[:search].has_key?(:for_day)
+        @current_date = params[:search][:for_day].respond_to?(:strftime) ?
+          params[:search][:for_day] : Time.zone.parse(params[:search][:for_day])
+      end
+    end
+
+    def empty_search?
+      return true unless params.has_key?(:search)
+      params[:search].values.all?(&:blank?)
+    end
+
+    def set_search_title
+      @search_title = "Search results for #{@current_date.present? ? '' : 'upcoming '}shows "
+
+      if params[:search][:for_day].present?
+        @search_title << %{on "#{@current_date.strftime("%A %B %d, %Y")}"}
+
+      elsif params[:search][:artists_name_starts_with]
+        @search_title << %{featuring artist matching "#{params[:search][:artists_name_starts_with]}"}
+      elsif params[:search][:venue_id_equals]
+        @search_title << %{at "#{Venue.find(params[:search][:venue_id_equals]).try(:name)}"}
       end
     end
 end
